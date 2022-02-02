ffe81fb6a6acbc9de37edcebd92e8343af3463aa
Dhruv Srivastava <dhruvdhs.ds@gmail.com>
2022-02-02 15:52:12 +0530

    generates all choices

--- sudoku.hs
+++ sudoku.hs
@@ -1,37 +1,72 @@
-
+main :: IO ()
+main = return ()
+-- import Data.List 
 
 -- Declare types : Sudoku Board, Rows, collums, boxes
 
-type Matrix a = [ [a] ]
+type Matrix a = [[a]]
 type Board = Matrix Char
+type Choices = [Char]
 
+boardSize = 9
+boxSize = 3
+cellvals = "123456789"
+blank e = e == '.'
+
+
+
+
+
+-- define a row, collumn and boxes
 rows :: Matrix a -> Matrix a
 rows = id
 
+
 cols :: Matrix a -> Matrix a
+cols [] = []
 cols [xs]  =  [[x] | x <-xs]
-cols (xs :: xss) zipWith (:) xs (cols xss)
+cols (xs : xss) = zipWith (:) xs (cols xss)
 
 boxs :: Matrix a -> Matrix a
-boxs = map ungroup . ungroup . map cols . group . map . group
+boxs x = map ungroup ( ungroup ( map cols (group  (map group x ))))
 
 
+
+-- group and return
 group :: [a] -> [[a]]
-group = groupBy boxsize
+group xs = groupBy  (floor (sqrt (fromIntegral (length xs)))) xs -- ToDo : Definition of groupBy
 
 ungroup :: [[a]] -> [a]
 ungroup = concat
 
+groupBy :: Int -> [a] -> [[a]]
+groupBy _ [] = []
+groupBy n xs = as : groupBy n bs
+  where (as,bs) = splitAt n xs
+
+
+-- nodups checks for any duplicates in a given list
 nodups :: Eq a => [a] -> Bool
 nodups [] = True
-nodups (x : xs) = notElem x xs ^ nodups xs
+nodups (x : xs) = not (x `elem` xs) && nodups xs
 
-correct Matrix a = [ [a] ]
-correct b = all nodups (rows b) ^
-  all nodups (cols b) ^
-  all nodups (boxs b)
+
+-- correct checks if filled board follows sudoku logic
+correct :: Board -> Bool 
+correct b = all nodups (rows b) &&  all nodups (cols b) &&  all nodups (boxs b)
 
 
 
 -- Function takes input board, returns list of possible solutions
 sudoku :: Board -> [Board]
+sudoku o = [o]
+
+
+
+
+
+-- generate choices for blank values
+choices :: Board -> Matrix Choices
+choices = map (map choose)
+choose e = if blank e then cellvals else [e]
+
